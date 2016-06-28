d114ed3bab32e235b45030d86e4e73800a86e729
Ubuntu <c.mikael.larsson@gmail.com>
2016-06-28 07:05:38 +0200

    Minor refactoring in org file. Added some more start up configurations.

--- init.el
+++ init.el
@@ -1,23 +1,3 @@
-;; hide menu bar, scrollbar and toolbar
-(menu-bar-mode 0)
-(tool-bar-mode 0)
-(scroll-bar-mode 0)
-
-;; do not show default startup screen
-(setq inhibit-startup-message t)
-
-;; highlight matching paranthesis
-(show-paren-mode 1)
-
-;; do not create annoying backup files
-(setq make-backup-files nil)
-
-;; turn off auto save mode
-(auto-save-mode 0)
-
-;; show line number
-(global-linum-mode 1)
-
 (require 'package)
 
 (setq package-list
